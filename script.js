e06af823971e1a86fa64507be7c407612da21da0
Dan Victor DEPED <danvictorprogramming1@gmail.com>
2022-04-24 19:14:04 +0800

    Added responsive JS on delete operators

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+// Calculator DOMs
 const numButton = document.querySelectorAll('.num');
+const delButton = document.querySelector('.del');
+const clearEntry = document.querySelector('.clear-entry');
 let screen = document.querySelector('.screen');
-screen.textContent ='';
+screen.textContent ='0';
+firstOperand = '';;
+secondOperand = '';
+// Event Listeners
+// Inputs Numbers on Screen
 numButton.forEach(button => {
     button.addEventListener('click', () => {
-        screen.textContent += `${button.textContent}`;
+        if (screen.textContent == '0') {
+            screen.textContent = `${screen.textContent.slice(0,-1)}`;
+        }
+        if (screen.textContent.length < 18) {
+            screen.textContent += `${button.textContent}`;
+        }
     });
 }); 
+// Special Operator
+// Deletes Input
+delButton.addEventListener('click', () => {
+    screen.textContent = `${screen.textContent.slice(0,-1)}`;
+});
+// Clears Input
+clearEntry.addEventListener('click', () => {
+    screen.textContent = '';
+});
+
